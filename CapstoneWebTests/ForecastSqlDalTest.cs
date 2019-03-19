38d9ab93daf849a104328e65dd0233f5a9283257
Darryl Baldwin <darrylbaldwin01@gmail.com>
2019-03-19 15:23:17 -0400

    Completed tests for park and forecast

---
+++ CapstoneWebTests/ForecastSqlDalTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Transactions;
+using Capstone.Web.DAL;
+using Capstone.Web.Models;
+
+namespace CapstoneWebTests
+{
+    [TestClass]
+    public class ForecastSqlDalTest
+    {
+        private TransactionScope transaction;
+
+        private string connectionString = @"Data Source=.\sqlexpress;Initial Catalog = NPGeek; Integrated Security = True";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            transaction = new TransactionScope();
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Test Park
+                    const string SQL_MakeTestPark = @"INSERT park VALUES('TEST', 'Test National Park', 'Ohio', 32832, 696, 125, 0, 'Woodland', 2000, 2189849, 'Of all the paths you take... ', 'John Muir', 'Though a short distance from the urban areas....', 8, 760);";
+                    SqlCommand command = new SqlCommand(SQL_MakeTestPark, connection);
+                    command.ExecuteNonQuery();
+
+                    //make 5 day forecast for test park
+                    const string SQL_MakeForecastDay1 = @"INSERT weather (parkCode, fiveDayForecastValue, low, high, forecast) VALUES('TEST', 1, 32, 51, 'partly cloudy')";
+                    command = new SqlCommand(SQL_MakeForecastDay1, connection);
+                    command.ExecuteNonQuery();
+
+                    const string SQL_MakeForecastDay2 = @"INSERT weather (parkCode, fiveDayForecastValue, low, high, forecast) VALUES('TEST', 2, 42, 72, 'sunny')";
+                    command = new SqlCommand(SQL_MakeForecastDay2, connection);
+                    command.ExecuteNonQuery();
+
+                    const string SQL_MakeForecastDay3 = @"INSERT weather (parkCode, fiveDayForecastValue, low, high, forecast) VALUES('TEST', 3, 26, 41, 'thunderstorms')";
+                    command = new SqlCommand(SQL_MakeForecastDay3, connection);
+                    command.ExecuteNonQuery();
+
+                    const string SQL_MakeForecastDay4 = @"INSERT weather (parkCode, fiveDayForecastValue, low, high, forecast) VALUES('TEST', 4, 40, 52, 'rain')";
+                    command = new SqlCommand(SQL_MakeForecastDay4, connection);
+                    command.ExecuteNonQuery();
+
+                    const string SQL_MakeForecastDay5 = @"INSERT weather (parkCode, fiveDayForecastValue, low, high, forecast) VALUES('TEST', 5, 14, 21, 'snow')";
+                    command = new SqlCommand(SQL_MakeForecastDay5, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Dispose();
+        }
+
+        [TestMethod()]
+        public void Get5DayForecastTest()
+        {
+            ForecastSqlDal dal = new ForecastSqlDal(connectionString);
+            List<Forecast> forecasts = dal.Get5DayForecast("TEST", true);
+
+            Assert.IsTrue(forecasts.Any(i => i.ParkCode == "TEST"));
+            Assert.IsTrue(forecasts.Count == 5);
+            Assert.IsFalse(forecasts.Any(i => i.TemperatureAdvice == null));
+            Assert.IsFalse(forecasts.Any(i => i.TemperatureAdvice == "Pack Snowshoes"));
+        }
+    }
+}
---
+++ CapstoneWebTests/ParkSqlDalTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Transactions;
+using Capstone.Web.DAL;
+using Capstone.Web.Models;
+
+namespace CapstoneWebTests
+{
+    [TestClass]
+    public class ParkSqlDalTest
+    {
+        private TransactionScope transaction;
+
+        private string connectionString = @"Data Source=.\sqlexpress;Initial Catalog = NPGeek; Integrated Security = True";
+
+        [TestInitialize]
+        public void Intialize()
+        {
+            transaction = new TransactionScope();
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    //make test parks
+                    const string SQL_MakeTestPark = @"INSERT park VALUES('TEST', 'Test National Park', 'Ohio', 32832, 696, 125, 0, 'Woodland', 2000, 2189849, 'Of all the paths you take... ', 'John Muir', 'Though a short distance from the urban areas....', 8, 760);";
+                    SqlCommand command = new SqlCommand(SQL_MakeTestPark, connection);
+                    command.ExecuteNonQuery();
+
+                    const string SQL_MakeTestParkII = @"INSERT park VALUES('TESTII', 'TEST National Park', 'Florida', 1508538, 0, 35, 0, 'Tropical', 1934, 1110901, 'There are no other Everglades..', 'Marjory Stoneman Douglas', 'The Florida Everglades...', 8, 760);";
+                    command = new SqlCommand(SQL_MakeTestParkII, connection);
+                    command.ExecuteNonQuery();                   
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Dispose();
+        }
+
+        [TestMethod()]
+        public void GetParksTest()
+        {
+            ParkSqlDal dal = new ParkSqlDal(connectionString);
+            List<Park> parks = dal.GetParks();
+
+            Assert.IsTrue(parks.Any(i => i.ParkCode == "TEST"));
+            Assert.IsTrue(parks.Any(i => i.Name == "Test National Park"));
+            Assert.IsTrue(parks.Count >= 2);
+        }
+
+        [TestMethod()]
+        public void GetParksDetailTest()
+        {
+            ParkSqlDal dal = new ParkSqlDal(connectionString);
+            Park park = dal.GetParkDetail("TEST");
+
+            Assert.IsNotNull(park.ParkCode);
+            Assert.IsTrue(park.Name == "Test National Park");
+            Assert.IsTrue(park.NumberOfAnimalSpecies == 760);
+        }
+    }
+}
